ac7398c28930d7788f15bc2d1182dbb4808c99ef
Owen <owen.petchey@ieu.uzh.ch>
2020-01-05 21:41:06 +0100

    extras subjects added

---
+++ _special_blocks.r
@@ -0,0 +1,15 @@
+```{block, type="effrel"}
+**Efficiency and reliability** In these we describe practices and methods for achieving higher efficiency and reliability in our journey from data to insights. They contain information about how to make our work more robust and reliable, such that it can still function if we get or add some new data, or otherwise make some changes in our work. And that conclusions/insights are robust. All this in a streamlined efficient workflow.
+```
+
+```{block, type="beaware"}
+**Be aware** These contain instructions about an opportunity/need to carefully consider an issue, for example a way to work that reduces the potential for mistakes, such as including appropriate checks and balances. These can also concern a warning or a common "gotcha". There are a number of common pitfalls that trip up new users of R (and more experienced users too!). We aim to highlight these and show you how to avoid them.
+```
+
+```{block, type="action"}
+**Action** A box containing instructions for you to do something important. Now!
+  ```
+
+```{block, type="info"}
+**Information** These aim to offer a not-too-technical discussion of how or why something works the way it does. You do not have to understand everything in these boxes to use R, but the information will help you understand how it works.
+```
